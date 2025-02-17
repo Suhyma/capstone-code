24da8af51b1974fc162234738cb93f80c146ab6e
sandrad125 <sanvis@live.com>
2025-02-16 19:26:24 -0500

    modified database API endpoint for entering patient info (C)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import AudioFileUploadView
+from .views import AudioFileUploadView, PatientCreateView, PatientDetailView, ScoreListView
 
 urlpatterns = [
-    path('upload/', AudioFileUploadView.as_view(), name='audio-file-upload'),
+    # path('upload/', AudioFileUploadView.as_view(), name='audio-file-upload'),
+    path('patients/', PatientCreateView.as_view(), name='patient-create'),
+    path('patients/<int:pk>/', PatientDetailView.as_view(), name='patient-detail'),
+    path('patients/<int:patient_id>/scores/', ScoreListView.as_view(), name='patient-scores'),
 ]
