0e4e178bf4aa3a2e8929a8902fbc9c1fe353a705
sandrad125 <sanvis@live.com>
2025-03-04 13:44:10 -0500

    hosted APIs prod

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-iwdsw@)x-$=8rf7as7p^8d1q2yvkk=a8j^b9l^kf#)(l-bzqr_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.36.104.178', '172.20.10.2', '10.2.0.2', '8bb3-2620-101-f000-7c0-00-1-30eb.ngrok-free.app']
 
 
 # Application definition
