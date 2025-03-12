1e77cb2f54e342ddb04d072f6c089cef3b97e1b6
sandrad125 <sanvis@live.com>
2025-03-12 13:17:43 -0400

    hosted apis again

--- frontend/SpeechBuds/services/api.ts
+++ frontend/SpeechBuds/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { AxiosError } from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = 'https://714a-24-114-29-182.ngrok-free.app/api/register/';  // backend registration URL
-const API_BASE_URL = "https://714a-24-114-29-182.ngrok-free.app/api"; // Change if using a different URL
+const API_URL = 'https://c043-2620-101-f000-7c0-00-1-30eb.ngrok-free.app/api/register/';  // backend registration URL
+const API_BASE_URL = "https://c043-2620-101-f000-7c0-00-1-30eb.ngrok-free.app/api"; // Change if using a different URL
 
 
 // Function to handle user registration
