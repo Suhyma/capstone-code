6dcc52843f1a65118dd3c5318b91c7d662d23845
sandrad125 <sanvis@live.com>
2025-04-07 15:14:07 -0400

    print pred

--- audio/phoneme_extraction.py
+++ audio/phoneme_extraction.py
@@ -77,6 +77,7 @@ def extract_phonemes(audiofile):
     # => should give 'b ɪ k ʌ z j u ɚ z s l i p ɪ ŋ ɪ n s t ɛ d ə v k ɔ ŋ k ɚ ɪ ŋ ð ə l ʌ v l i ɹ z p ɹ ɪ n s ə s h æ z b ɪ k ʌ m ə v f ɪ t ə l w ɪ θ n b oʊ p ɹ ə ʃ æ ɡ i s ɪ t s ð ɛ ɹ ə k u ɪ ŋ d ʌ v'
     
     # get rid of duplicate phonemes at the start that cause scoring issues
+    print("Original phonemes: " + str(prediction))
     if len(prediction) >= 2 and prediction[0] == prediction[1]:
         prediction.pop(0)
 
