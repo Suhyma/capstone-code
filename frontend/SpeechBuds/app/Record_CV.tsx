6ff131652b4fbc94739def3c34e5cfbab23f03f9
sandrad125 <sanvis@live.com>
2025-04-12 14:09:42 -0400

    apis

--- frontend/SpeechBuds/app/Record_CV.tsx
+++ frontend/SpeechBuds/app/Record_CV.tsx
@@ -132,7 +132,7 @@ export default function Record() {
       console.log("Sending video URL to backend...");
   
       const response = await axios.post(
-        "https://cdaa-24-114-29-182.ngrok-free.app/api/submit_audio/",
+        "https://0821-72-138-72-162.ngrok-free.app/api/submit_audio/",
         formData,
         {
           headers: {
